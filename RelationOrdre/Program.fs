a4629ed95396a607136903560c480e9d2ab5e9e7
Bruno Cadorette <brunocadorette@outlook.com>
2013-11-23 16:41:10 -0500

    Rangs et matrice c
    Conversion matrice m en matrice c
    Ajout des rangs
    Optimisation de l'affichage console d'une matrice

--- RelationOrdre/Program.fs
+++ RelationOrdre/Program.fs
@@ -1,7 +1,4 @@
-﻿// Obtenez des informations sur F# via http://fsharp.net
-// Voir le projet 'Didacticiel F#' pour obtenir de l'aide.
-
-open System;
+﻿open System;
 
 type regleRelation<'T> = 'T->'T->bool
 
@@ -9,27 +6,40 @@ type regleRelation<'T> = 'T->'T->bool
 module Array2D =
     let getRow (i:int)(array:'T[,]) = 
         array.[i..i, *] |> Seq.cast<'T>
+    
     let getColumn (i:int)(array:'T[,]) = 
         array.[*, i..i] |> Seq.cast<'T>
 
 type Relation<'T> = {
     elements:'T[]
     regle:regleRelation<'T>
-    matrice:bool[,]
+    matriceM:bool[,]
+    matriceC:bool[,]
 }
 
 type Relation<'T> with
     static member init<'T> (initializer:regleRelation<'T>) (elements:'T[]) =
         let matrice = Array2D.init elements.Length elements.Length (fun i j->initializer elements.[i] elements.[j])
-        {elements=elements; regle= initializer; matrice=matrice}
+        {elements=elements; regle= initializer; matriceM=matrice; matriceC=Relation<'T>.initCMatrice matrice}
 
+    static member private initCMatrice (matrice:bool[,]) =
+        let n = matrice.GetUpperBound(0)
+        let mEnC i j =
+            if (i=j) then
+                true
+            else if (matrice.[i,j]) then
+                not (seq {0..n} |> Seq.exists(fun k-> i<>k && j<>k && matrice.[i,k] && matrice.[k,j]))
+            else
+                false
+        Array2D.init (n+1) (n+1) mEnC
+        
     static member private trouver (selecteur:int->bool[,]->seq<bool>)(relation:Relation<'T>) =
-        [|0..relation.matrice.GetUpperBound(0)|] 
-        |> Array.filter(fun i->relation.matrice |> selecteur(i) |> Seq.mapi(fun j x->(x,j)) |> Seq.forall(fun (x,j)-> not x || i=j)) 
+        [|0..relation.matriceM.GetUpperBound(0)|] 
+        |> Array.filter(fun i->relation.matriceM |> selecteur(i) |> Seq.mapi(fun j x->(x,j)) |> Seq.forall(fun (x,j)-> not x || i=j)) 
         |> Array.map(fun i->relation.elements.[i])
 
     static member private unique (selecteur:int->bool[,]->seq<bool>) (relation:Relation<'T>) =
-        let indice = [|0..relation.matrice.GetUpperBound(0)|] |> Array.tryFind(fun i->relation.matrice |> selecteur(i) |> Seq.forall(fun x->x))
+        let indice = [|0..relation.matriceM.GetUpperBound(0)|] |> Array.tryFind(fun i->relation.matriceM |> selecteur(i) |> Seq.forall(fun x->x))
         match indice with
         | Some n -> Some(relation.elements.[n])
         | None   -> None
@@ -37,22 +47,23 @@ type Relation<'T> with
     //TODO: Mettre les fonctions de tests comme ça encore plus généric comme pour les minimaux/maximaux et pour le minimum/maximum
     //TODO: Trouver un meilleur moyen pour générer les tuples
     static member estReflexif (relation:Relation<'T>) =
-        seq { 0..relation.matrice.GetUpperBound(0) } |> Seq.forall(fun i-> relation.matrice.[i,i])
+        seq { 0..relation.matriceM.GetUpperBound(0) } |> Seq.forall(fun i-> relation.matriceM.[i,i])
 
     static member estSymetrique (relation:Relation<'T>) =
-        let n = relation.matrice.GetUpperBound(0)
+        let n = relation.matriceM.GetUpperBound(0)
         seq { for i in [|0..n|] do for j in  [|0..n|] do yield (i,j) }
-        |> Seq.forall(fun (i,j)->relation.matrice.[i,j]=relation.matrice.[j,i])
+        |> Seq.forall(fun (i,j)->relation.matriceM.[i,j]=relation.matriceM.[j,i])
 
     static member estAntiSymetrique (relation:Relation<'T>) =
-        let n = relation.matrice.GetUpperBound(0)
+        let n = relation.matriceM.GetUpperBound(0)
         seq { for i in [|0..n|] do for j in  [|0..n|] do yield (i,j) }
-        |> Seq.forall(fun (i,j)->i<>j||Convert.ToInt32(relation.matrice.[i,j])+Convert.ToInt32(relation.matrice.[j,i])>=1)
+        |> Seq.forall(fun (i,j)->i<>j||Convert.ToInt32(relation.matriceM.[i,j])+Convert.ToInt32(relation.matriceM.[j,i])>=1)
 
+    //A optimiser
     static member estTransitif (relation:Relation<'T>) = 
-        let n = relation.matrice.GetUpperBound(0)
+        let n = relation.matriceM.GetUpperBound(0)
         seq { for i in [|0..n|] do for j in  [|0..n|] do for k in [|0..n|] do yield (i,j,k) }
-        |> Seq.forall(fun (i,j,k)->(not relation.matrice.[i,j] || not relation.matrice.[j,k]) || (relation.matrice.[i,j] && relation.matrice.[j,k] && relation.matrice.[i,k]))
+        |> Seq.forall(fun (i,j,k)->(not relation.matriceM.[i,j] || not relation.matriceM.[j,k]) || (relation.matriceM.[i,j] && relation.matriceM.[j,k] && relation.matriceM.[i,k]))
 
     static member estRelationOrdre (relation:Relation<'T>) =
         Relation.estReflexif relation && Relation.estAntiSymetrique relation && Relation.estTransitif relation
@@ -72,11 +83,29 @@ type Relation<'T> with
     static member maximum (relation:Relation<'T>) =
         Relation.unique Array2D.getColumn relation
           
+
+    
+    static member rangs (relation:Relation<'T>) =
+        let rec trouverRangs (rangs:(int*int[])list) (rangActuel:int) (ligneDelete:int[]) =
+            let func i = 
+                relation.matriceC 
+                |> Array2D.getColumn(i) 
+                |> Seq.mapi(fun i x->(i,x)) 
+                |> Seq.sumBy(fun (i,x)->Convert.ToInt32(not(ligneDelete|>Array.exists(fun y->y=i))&& x))=1
+
+            let n = relation.matriceC.GetUpperBound(0)
+            if (ligneDelete.Length<n) then
+                let courant = [|0..n|] |> Array.filter(func)
+                let predicat = fun (x,i)->not(courant |> Array.exists(fun x->x=i))
+                let t = rangActuel, courant
+                trouverRangs (t::rangs) (rangActuel+1) (Array.append courant ligneDelete)
+            else 
+                rangs
+        trouverRangs []  0 [||]
+
 let PrintMatrice (matrice: bool[,]) =
     for i in 0..(matrice|>Array2D.length1)-1 do
-        for j in 0..(matrice|>Array2D.length2)-1 do
-            Console.Write(Convert.ToInt32(matrice.[i,j]))
-        Console.WriteLine();
+        Console.WriteLine(String.Join("",matrice |> Array2D.getRow(i) |> Seq.map(fun x->Convert.ToInt32(x))))
         
 let IsIn a b=
     a |> Seq.forall(fun x->b|>Seq.exists(fun y->y=x))
@@ -85,15 +114,15 @@ let IsIn a b=
 let powerset xs = 
     List.foldBack (fun x rest -> rest @ List.map (fun ys -> x::ys) rest) xs [[]]
 
-//TODO: Transformer la matrice M en matrice C
 //TODO: Faire le diagramme de Hasse
 [<EntryPoint>]
 let main argv = 
-    let elements = powerset [1..5]|> Seq.toArray
-    
+    let sets = powerset [1..6]|> Seq.toArray
+    let elements = [|0..20|]
+
     let divide = fun a b->(a<>0 && b % a = 0)||(a=b && a=0)
     printfn "%A" elements
-    let relation = Relation.init (fun a b->IsIn a b) elements
+    let relation = Relation.init (fun a b->divide a b) elements
     printfn "Les maximaux sont : %A" (relation |> Relation.maximaux )
     printfn "Les minimaux sont : %A" (relation |> Relation.minimaux )
     if (Relation.estRelationEquivalence relation) then
@@ -102,7 +131,10 @@ let main argv =
         printfn "Le maximum est : %A" (relation |> Relation.maximum )
         printfn "Le minimum est : %A" (relation |> Relation.minimum )
         printfn "Relation d'ordre : %A" (relation |> Relation.estRelationOrdre )
-    printfn "La matrice est : "
-    PrintMatrice(relation.matrice)
+        printfn "Les rangs sont : %A" (relation |> Relation.rangs )
+    printfn "La matrice M est : "
+    PrintMatrice(relation.matriceM)
+    printfn "La matrice C est : "
+    PrintMatrice(relation.matriceC)
     Console.ReadKey()|>ignore
     0 // retourne du code de sortie entier
